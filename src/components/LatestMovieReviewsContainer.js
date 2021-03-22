a0a2586e4d22077dd2742d787368ebd1fcd0f134
Adan Escamilla <adanescamilla580@gmail.com>
2021-03-21 21:48:15 -0700

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,38 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+    
+    constructor(){
+        super();
+        this.state = {
+            reviews: []
+        }
+    }
+    //has a state set to an empty array
+
+
+    componentDidMount() {
+        fetch(URL)
+        .then(res => res.json())
+        .then(res => this.setState({ reviews: res.value }))
+    }
+    //it fetches data from the url and sets that data to the initial state.
+    //WE use a componentDidMount for our fetch.
+
+    render() {
+        return (
+          <div className="latest-movie-reviews">
+            <h2>The Latest Reviews:</h2>
+            <MovieReviews reviews={this.state.reviews} />
+          </div>
+        );
+      }
+      //we make a div with a class name
+      //we make a title with an h2
+      //we then pass in the current state down to MovieReviews
+      //all this component does is make the fetch and stores that data in the state.
+}
+
+export default LatestMovieReviewsContainer;
